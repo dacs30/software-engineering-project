15ae497f5fa01b10cb7dad7af88ea9ffccd705af
nikopoulospet <xxpeteynikxx@gmail.com>
2021-04-07 11:42:58 -0400

    fixed file paths

--- src/main/java/edu/wpi/MochaManticores/database/EdgeManager.java
+++ src/main/java/edu/wpi/MochaManticores/database/EdgeManager.java
@@ -1,6 +1,8 @@
 package edu.wpi.MochaManticores.database;
 
 public class EdgeManager {
+    private static final String Edge_csv_path = "data/edges.csv";
+    private static final String Node_csv_path = "data/nodes.csv";
     public static void showEdgeInformation(String edgeInfo) {
         System.out.println(edgeInfo);
     }
--- src/main/java/edu/wpi/MochaManticores/database/NodeManager.java
+++ src/main/java/edu/wpi/MochaManticores/database/NodeManager.java
@@ -4,25 +4,24 @@ import java.io.FileNotFoundException;
 import java.sql.*;
 
 public class NodeManager {
+    private static final String Node_csv_path = "data/nodes.csv";
 
     public static void updateNode(Connection connection, String id,  int xcoord, int ycoord) throws SQLException, FileNotFoundException {
-        String path = System.getProperty("user.dir");
         PreparedStatement pstmt = connection.prepareStatement("UPDATE NODES SET xcoord=?, ycoord=? WHERE nodeID=?");
         pstmt.setInt(1, xcoord);
         pstmt.setInt(2, ycoord);
         pstmt.setString(3, id);
         pstmt.executeUpdate();
-        CSVmanager nodeCSV = new CSVmanager(path + "/src/main/java/edu/wpi/MochaManticores/database/nodes.csv");
+        CSVmanager nodeCSV = new CSVmanager(Node_csv_path);
         nodeCSV.save_node_csv(connection);
     }
 
     public static void updateNodeName(Connection connection, String id, String newName) throws SQLException, FileNotFoundException {
-        String path = System.getProperty("user.dir");
         PreparedStatement pstmt = connection.prepareStatement("UPDATE NODES SET longName=? WHERE nodeID=?");
         pstmt.setString(1, newName);
         pstmt.setString(2, id);
         pstmt.executeUpdate();
-        CSVmanager nodeCSV = new CSVmanager(path + "/src/main/java/edu/wpi/MochaManticores/database/nodes.csv");
+        CSVmanager nodeCSV = new CSVmanager(Node_csv_path);
         nodeCSV.save_node_csv(connection);
     }
 
