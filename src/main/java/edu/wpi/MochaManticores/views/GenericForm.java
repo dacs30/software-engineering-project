82ed40678378a51b50dca9c4ed32871dee10b883
kush5683 <32845063+kush5683@users.noreply.github.com>
2021-04-09 12:22:03 -0400

    Added barebones code to GenericForm.java

--- src/main/java/edu/wpi/MochaManticores/views/GenericForm.java
+++ src/main/java/edu/wpi/MochaManticores/views/GenericForm.java
@@ -1,4 +1,47 @@
 package edu.wpi.MochaManticores.views;
 
-public class GenericForm {
+import com.jfoenix.controls.*;
+import javafx.fxml.FXML;
+import javafx.scene.paint.Paint;
+
+import javafx.event.ActionEvent;
+
+public class GenericForm extends SceneController{
+
+    @FXML
+    private JFXTextField fieldOne;
+
+    @FXML
+    private JFXTextArea fieldThree;
+
+    @FXML
+    private JFXComboBox box;
+
+    @FXML
+    private JFXRadioButton rbOne;
+
+    @FXML
+    private JFXRadioButton rbTwo;
+
+    @FXML
+    private JFXRadioButton rbThree;
+
+    public void initialize() {
+        box.getItems().clear();
+        box.getItems().addAll("1", "2", "3");
+    }
+
+    public void submit(ActionEvent e){
+        System.out.println(fieldOne.getText());
+        System.out.println(box.getSelectionModel().getSelectedItem());
+        System.out.println(fieldThree.getText());
+        System.out.println(rbOne.isSelected());
+
+        returnToMain(e);
+    }
+
+    public void cancel(ActionEvent e) {
+        System.out.println("Form cancelled");
+        returnToMain(e);
+    }
 }
