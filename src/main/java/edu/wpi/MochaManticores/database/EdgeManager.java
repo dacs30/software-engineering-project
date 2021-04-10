907940c2705c55ed044a6d661fdc364f7f43e261
nikopoulospet <xxpeteynikxx@gmail.com>
2021-04-10 16:07:52 -0400

    added methods to add nodes and edges to database

--- src/main/java/edu/wpi/MochaManticores/database/EdgeManager.java
+++ src/main/java/edu/wpi/MochaManticores/database/EdgeManager.java
@@ -1,8 +1,27 @@
 package edu.wpi.MochaManticores.database;
 
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class EdgeManager {
     private static final String Edge_csv_path = "data/edges.csv";
-    private static final String Node_csv_path = "data/nodes.csv";
+
+    public static void createEdge(Connection connection, String Node1, String Node2) throws SQLException, FileNotFoundException {
+        String edgeId = Node1 + "_" + Node2;
+        String sql = "INSERT INTO EDGES (edgeID, startNode, endNode) " +
+                "VALUES (?, ?, ?)";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        pstmt.setString(1, edgeId);
+        pstmt.setString(2, Node1);
+        pstmt.setString(3, Node2);
+        pstmt.executeUpdate();
+
+        CSVmanager nodeCSV = new CSVmanager(Edge_csv_path);
+        nodeCSV.save_node_csv(connection);
+    }
+
     public static void showEdgeInformation(String edgeInfo) {
         System.out.println(edgeInfo);
     }
