7f60e467804cd126d7b065b5d24ca77da1571c3f
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2021-04-11 17:52:58 -0400

    Login page added as initial page. Still work in progress.

---
+++ src/main/java/edu/wpi/MochaManticores/views/LoginPage.java
@@ -0,0 +1,10 @@
+package edu.wpi.MochaManticores.views;
+
+import javafx.event.ActionEvent;
+
+public class LoginPage extends SceneController{
+
+    public void onMouseClickedContinue(ActionEvent e) {
+        changeSceneTo(e, "landingPage");
+    };
+}
