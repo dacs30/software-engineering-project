b361502776b687e706bff45061be74aa0b2fb493
kush5683 <32845063+kush5683@users.noreply.github.com>
2021-04-12 19:38:01 -0400

    Added fullscreen to landingPage

--- src/main/java/edu/wpi/MochaManticores/views/landingPageController.java
+++ src/main/java/edu/wpi/MochaManticores/views/landingPageController.java
@@ -5,31 +5,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 
 public class landingPageController extends SceneController {
 
 
-  @FXML private AnchorPane confirmEmergencyDialog;
 
-  @FXML private AnchorPane outerContainer;
-
-  @FXML private AnchorPane innerContainer;
-
-  @FXML private Rectangle titleBG;
+  @FXML private GridPane contentPane;
 
 
   @FXML private void initialize(){
-    double h = App.getPrimaryStage().getScene().getWindow().getHeight();
-    double w = App.getPrimaryStage().getScene().getWindow().getWidth();
-    double height = innerContainer.getPrefHeight();
-    double width = innerContainer.getPrefWidth();
-    double setX = (w/2) - (width/2);
-    double setY = (h/2) - (height/2);
-    System.out.println(setX);
-    System.out.println(setY);
-    innerContainer.setLayoutX(setX);
-    innerContainer.setLayoutY(setY);
+      double height = super.getHeight();
+      double width = super.getWidth();
+      contentPane.setPrefSize(width,height);
   }
 
   @FXML
@@ -44,12 +33,10 @@ public class landingPageController extends SceneController {
 
   @FXML
   private void dismissEmergencyDialog(ActionEvent e){
-    confirmEmergencyDialog.setVisible(false);
   }
 
   @FXML
   private void confirmEmergency(ActionEvent e){
-    confirmEmergencyDialog.setVisible(true);
   }
 
   @FXML
@@ -77,17 +64,12 @@ public class landingPageController extends SceneController {
     changeSceneTo(e,"sanitationService");
   }
 
-  @FXML
-  private void scaleScene(MouseEvent e){
-    double h = App.getPrimaryStage().getScene().getWindow().getHeight();
-    double w = App.getPrimaryStage().getScene().getWindow().getWidth();
-    double height = outerContainer.getPrefHeight();
-    double width = outerContainer.getPrefWidth();
-    double setX = (w/2) - (width/2);
-    double setY = (h/2) - (height/2);
-    System.out.println(setX);
-    System.out.println(setY);
-    outerContainer.setLayoutX(setX);
-    outerContainer.setLayoutY(setY);
+  public void back(ActionEvent e){
+    super.back(e);
   }
+
+  public void exitApp(ActionEvent e){
+    super.exitApp(e);
+  }
+
 }
