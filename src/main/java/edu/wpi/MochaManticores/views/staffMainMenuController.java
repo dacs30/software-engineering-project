d710d2e7f2062a244ff862fef5ea55cb15178e45
hmkyriacou <hmkyriacou14@gmail.com>
2021-04-13 15:09:46 -0400

    downloading both csv files

--- src/main/java/edu/wpi/MochaManticores/views/staffMainMenuController.java
+++ src/main/java/edu/wpi/MochaManticores/views/staffMainMenuController.java
@@ -1,9 +1,17 @@
 package edu.wpi.MochaManticores.views;
 
+import edu.wpi.MochaManticores.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class staffMainMenuController extends SceneController{
 
@@ -28,6 +36,34 @@ public class staffMainMenuController extends SceneController{
 
     public void gotoNode(ActionEvent e){
         super.changeSceneTo("mapPage");
+    }
+
+    public void downloadCSVs(ActionEvent e) {
+        String path = getPath();
+        if(path.equals("")){
+
+        }else{
+            File dst = new File(path + "\\MapMNodes.csv");
+            File dst2 = new File(path + "\\MapMEdges.csv");
+            try{
+                File source = new File("data/MapMNodes.csv");
+                File source2 = new File("data/MapMEdges.csv");
+                Files.copy(source.toPath(),dst.toPath(),REPLACE_EXISTING);
+                Files.copy(source2.toPath(),dst2.toPath(),REPLACE_EXISTING);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    public String getPath() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(App.getPrimaryStage());
+        if(selectedDirectory!=null){
+            String path = selectedDirectory.getAbsolutePath();
+            return  selectedDirectory.getAbsolutePath();//TODO: check windows or UNIX and start at ~/Downloads or $USER/downloads
+        }
+        return "";
     }
 
     public void back(){
