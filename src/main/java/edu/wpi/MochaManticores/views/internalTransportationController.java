19e10320196cfb35dddc8c8075e87d57caf1c0b2
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2021-04-14 16:06:26 -0400

    Internal transportation re-design with jfoenix pop up

--- src/main/java/edu/wpi/MochaManticores/views/internalTransportationController.java
+++ src/main/java/edu/wpi/MochaManticores/views/internalTransportationController.java
@@ -1,7 +1,5 @@
 package edu.wpi.MochaManticores.views;
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.*;
 import edu.wpi.MochaManticores.App;
 import java.awt.*;
 import java.io.IOException;
@@ -14,6 +12,12 @@ import javafx.scene.Parent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
 public class internalTransportationController extends SceneController{
 
 
@@ -22,40 +26,75 @@ public class internalTransportationController extends SceneController{
 
     // Information of Patient/One Being Transported
     @FXML
-        private JFXTextField patientID;
+    private JFXTextField patientID;
     @FXML
-        private JFXTextField numberOfStaff;
+    private JFXTextField numberOfStaff;
     @FXML
-        private JFXTextField destination;
+    private JFXTextField destination;
     @FXML
-        private JFXComboBox transportComboBox;
+    private JFXComboBox transportComboBox;
+    @FXML
+    private GridPane contentGrid;
+    @FXML
+    private ImageView backgroundIMG;
+    @FXML
+    private StackPane dialogPane;
 
-    // Buttons for Form
     @FXML
-        private JFXButton cancelButton;
-    @FXML
-        private JFXButton submitButton;
-    @FXML
-        private void initialize() {
+    private void initialize() {
+        double height = App.getPrimaryStage().getScene().getHeight();
+        double width = App.getPrimaryStage().getScene().getWidth();
+        backgroundIMG.setFitHeight(height);
+        backgroundIMG.setFitWidth(width);
+        contentGrid.setPrefSize(width,height);
+
         transportComboBox.setItems(typeOfTransportList);
-        transportComboBox.setValue("Wheelchair");
+
+        dialogPane.setDisable(false);
     }
-    @FXML
-    private void cancelScene(ActionEvent e) {
-super.back();
+
+    public void goBack(ActionEvent actionEvent) {
+        back();
     }
-    @FXML
-        private void advanceSuccessScene(ActionEvent e) {
-        System.out.println(patientID.getText());
-        System.out.println(numberOfStaff.getText());
-        System.out.println(destination.getText());
-        System.out.println(transportComboBox.getSelectionModel().getSelectedItem());
-        super.changeSceneTo("successPage");
-//            try {
-//                Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/MochaManticores/fxml/successPage.fxml"));
-//                App.getPrimaryStage().getScene().setRoot(root);
-//            } catch (IOException ex) {
-//            ex.printStackTrace();
-//            }
+
+    public void submitEvent(ActionEvent actionEvent) {
+        loadSubmitDialog();
+    }
+
+    public void loadSubmitDialog(){
+        //TODO Center the text of it.
+        dialogPane.toFront();
+        dialogPane.setDisable(false);
+        JFXDialogLayout message = new JFXDialogLayout();
+        message.setMaxHeight(Region.USE_PREF_SIZE);
+        message.setMaxHeight(Region.USE_PREF_SIZE);
+
+        final Text hearder = new Text("Your request was submited");
+        hearder.setStyle("-fx-font-weight: bold");
+        hearder.setStyle("-fx-font-size: 30");
+        hearder.setStyle("-fx-font-family: Roboto");
+        hearder.setStyle("-fx-alignment: center");
+        message.setHeading(hearder);
+
+        final Text body = new Text("Estimated time for arrival: ");
+        body.setStyle("-fx-font-size: 15");
+        body.setStyle("-fx-font-family: Roboto");
+        body.setStyle("-fx-alignment: center");
+        message.setHeading(hearder);
+
+        message.setBody(body);
+        JFXDialog dialog = new JFXDialog(dialogPane, message,JFXDialog.DialogTransition.CENTER);
+        JFXButton ok = new JFXButton("OK");
+        ok.setOnAction(event -> {
+            changeSceneTo("landingPage");
+        });
+
+        dialog.setOnDialogClosed(event -> {
+            dialogPane.toBack();
+            dialog.close();
+        });
+
+        message.setActions(ok);
+        dialog.show();
     }
 }
