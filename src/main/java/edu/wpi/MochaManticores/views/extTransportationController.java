2539dff9f7129a67051bd5f26589c0b2abfc8fd9
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2021-04-14 11:27:50 -0400

    Fixed landing page, external page now has a new design and a dialog box

--- src/main/java/edu/wpi/MochaManticores/views/extTransportationController.java
+++ src/main/java/edu/wpi/MochaManticores/views/extTransportationController.java
@@ -1,5 +1,8 @@
 package edu.wpi.MochaManticores.views;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
 import edu.wpi.MochaManticores.App;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,37 +12,81 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
-import java.io.IOException;
-
-public class extTransportationController {
+public class extTransportationController extends SceneController {
 
     ObservableList<String> transportationMethod = FXCollections.observableArrayList("Ambulance", "Helicopter", "Plane");
 
     @FXML
-    private TextField patientName;
+    private GridPane contentGrid;
     @FXML
-    private TextField roomNumber;
+    private ImageView backgroundIMG;
     @FXML
-    private TextField address;
+    private ComboBox<String> transportationMethods;
     @FXML
-    private ComboBox<String> transportationDropDown;
+    private StackPane dialogPane;
 
     @FXML
     private void initialize() {
-        //transportationDropDown.setValue("Ambulance");
-        transportationDropDown.setItems(transportationMethod);
+        double height = App.getPrimaryStage().getScene().getHeight();
+        double width = App.getPrimaryStage().getScene().getWidth();
+        backgroundIMG.setFitHeight(height);
+        backgroundIMG.setFitWidth(width);
+        contentGrid.setPrefSize(width,height);
 
+        transportationMethods.setItems(transportationMethod);
+
+        dialogPane.setDisable(false);
     }
 
-    @FXML
-    private void advanceScene(ActionEvent e) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/MochaManticores/fxml/landingPage.fxml"));
-            App.getPrimaryStage().getScene().setRoot(root);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+
+    public void backEvent(ActionEvent actionEvent) {
+        changeSceneTo("landingPage");
     }
 
+    public void submitEvent(ActionEvent actionEvent) {
+        loadSubmitDialog();
+    }
+
+    public void loadSubmitDialog(){
+        //TODO Center the text of it.
+        dialogPane.toFront();
+        dialogPane.setDisable(false);
+        JFXDialogLayout message = new JFXDialogLayout();
+        message.setMaxHeight(Region.USE_PREF_SIZE);
+        message.setMaxHeight(Region.USE_PREF_SIZE);
+
+        final Text hearder = new Text("Your request was submited");
+        hearder.setStyle("-fx-font-weight: bold");
+        hearder.setStyle("-fx-font-size: 30");
+        hearder.setStyle("-fx-font-family: Roboto");
+        hearder.setStyle("-fx-alignment: center");
+        message.setHeading(hearder);
+
+        final Text body = new Text("Estimated time for arrival: ");
+        body.setStyle("-fx-font-size: 15");
+        body.setStyle("-fx-font-family: Roboto");
+        body.setStyle("-fx-alignment: center");
+        message.setHeading(hearder);
+
+        message.setBody(body);
+        JFXDialog dialog = new JFXDialog(dialogPane, message,JFXDialog.DialogTransition.CENTER);
+        JFXButton ok = new JFXButton("OK");
+        ok.setOnAction(event -> {
+            changeSceneTo("landingPage");
+        });
+
+        dialog.setOnDialogClosed(event -> {
+            dialogPane.toBack();
+            dialog.close();
+        });
+
+        message.setActions(ok);
+        dialog.show();
+    }
 }
