15a9d298f86006d32ffd70e50424f583a4df4811
atkneedler <atkneedler@wpi.edu>
2021-04-26 15:40:44 -0400

    Added GBF(depth first) and Dijkstra(breadth first) and combined all pathplanning classes under the PathPlanning interface. -Added constructors to AStarNode.java to facilitate different pathplanning algos -Note: Algorithms share enough code that it may be possible to consolidate them into a single class in the future.

--- src/main/java/edu/wpi/MochaManticores/Algorithms/Dijkstra.java
+++ src/main/java/edu/wpi/MochaManticores/Algorithms/Dijkstra.java
@@ -8,11 +8,10 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 /**
- * AStar implementation using a priority queue to find the most efficient path to the target node
+ * Dijkstra's algorithm implementation using a priority queue to find the most efficient path to the target node
  * @author aksil
- * @author gatch
  */
-public class AStar2 implements PathPlanning{
+public class Dijkstra implements PathPlanning{
     //Declare instance variables
     private HashMap<String, NodeSuper> nodes;           //The HashMap containing all nodes on the map
     private PriorityQueue<AStarNode> horizon;           //Contains unvisited nodes adjacent to visited ones (lowest cost first)
@@ -20,11 +19,10 @@ public class AStar2 implements PathPlanning{
     private HashMap<String, AStarNode> horizonNodes;    //Same as horizon but as a list to enable object retrieval
     private AStarNode currentNode;                      //Current node being explored
     private NodeSuper target;                           //The node being searched for
-    private String targetType;                          //The type of node being searched for (findNearest)
     private String condition;                           //Conditions which must be met by nodes in the path
 
     //Constructor
-    public AStar2() {
+    public Dijkstra() {
         this.nodes = MapSuper.getMap();
     }
 
@@ -37,7 +35,7 @@ public class AStar2 implements PathPlanning{
      * @return path LinkedList containing all node IDs in the route
      */
     @Override
-    public LinkedList<String> multiStopRoute(LinkedList<NodeSuper> stops, String condition){
+    public LinkedList<String> multiStopRoute(LinkedList<NodeSuper> stops, String condition) {
         this.condition = condition;
         LinkedList<String> path = new LinkedList<>();
         if (stops.size() == 1){
@@ -56,8 +54,7 @@ public class AStar2 implements PathPlanning{
 
     /**
      * method: findRoute()
-     * usage: Uses an A* algorithm to calculate the most efficient route to the target
-     *        with exploration guided by GBF
+     * usage: Uses Dijkstra's algorithm to calculate the most efficient route to the target
      * @param start the node of origin
      * @param target the node to search for
      * @return a LinkedList of Strings containing the IDs of the nodes from the first node to the target
@@ -68,7 +65,7 @@ public class AStar2 implements PathPlanning{
         this.horizon = new PriorityQueue<AStarNode>(10, new NodeComparator());  //Sorts using NodeComparator
         this.visitedNodes = new HashMap<>();                                                //Initialized as empty
         this.horizonNodes = new HashMap<>();                                                //Initialized as empty
-        this.currentNode = new AStarNode(start, target, "NONE", 0);         //Initialized to start node
+        this.currentNode = new AStarNode(start, "NONE", 0);         //Initialized to start node
         this.target = target;                           //Initializes the target variable
 
         //Initialize local variables
@@ -79,7 +76,7 @@ public class AStar2 implements PathPlanning{
         while(true) {
             //checkNeighbors adds to the horizon and compares routes, returns false unless the target is found
             this.visitedNodes.put(this.currentNode.getID(), this.currentNode);
-            if(!checkNeighbors(true).equals("false")) {
+            if(checkNeighbors()) {
                 break;
             }
             this.currentNode = this.horizon.poll();
@@ -102,13 +99,12 @@ public class AStar2 implements PathPlanning{
     /**
      * method: checkNeighbors()
      * usage: explores the neighbors of the currentNode, updating path costs and adding to the horizon
-     * @param targetNode true if the function is searching for a specific node, false if searching for a nodeType
      * @return a string containing either the target node ID or "false" if the target has not been found
      */
-    private String checkNeighbors(boolean targetNode) {
+    private boolean checkNeighbors() {
         //Initialize local variables
         String currentID = this.currentNode.getID();
-        String located = "false";
+        boolean located = false;
 
         //Loop through all nodes connected to the current node
         for(String ID : this.nodes.get(currentID).getNeighbors()) {
@@ -136,17 +132,8 @@ public class AStar2 implements PathPlanning{
             //If the node is accessible, continue
             if(isAccessible) {
                 int travelCost = this.currentNode.getCost() + neighbor.getCost(currentID);  //cost from start -> neighbor
-                AStarNode newNode;
 
-                //If searching for a specific node use A*
-                if(targetNode) {
-                    newNode = new AStarNode(neighbor, this.target, currentID, travelCost);    //Makes a new A* node
-                }
-
-                //Otherwise, use dijkstra's to search (0 estimated cost to target)
-                else {
-                    newNode = new AStarNode(neighbor, this.nodes.get(ID), currentID, travelCost);
-                }
+                AStarNode newNode = new AStarNode(neighbor, currentID, travelCost);    //Makes a new A* node
 
                 //If the node has already been visited, see if the new path is faster
                 if(this.visitedNodes.containsKey(ID)) {
@@ -167,83 +154,17 @@ public class AStar2 implements PathPlanning{
                 else {
                     this.horizon.add(newNode);
                     this.horizonNodes.put(ID, newNode);
-                    if(targetNode) {
+
                         if(neighbor.getID().equals(this.target.getID())) {
                             //Before the loop breaks, add the target node to visitedNodes (prevents an issue in the next loop)
                             this.visitedNodes.put(ID, newNode);
                             //Set the found flag to break the search loop
-                            located = ID;
+                            located = true;
                         }
-                    }
-                    else if(neighbor.getType().equals(targetType)) {
-                        //Before the loop breaks, add the target node to visitedNodes (prevents an issue in the next loop)
-                        this.visitedNodes.put(ID, newNode);
-                        //Set the found flag to break the search loop
-                        located = ID;
                     }
                 }
             }
-        }
         //return true if one of the neighbors is the target, false otherwise
         return located;
-    }
-
-    /**
-     * method: findNearest()
-     * usage: uses dijkstra's algorithm to find a rout to the nearest node of a certain type
-     * @param start the origin node
-     * @param targetType the nodeType being searched for
-     * @param condition condition code for the path "publicOnly, handicap, none, publicHandicap"
-     * @return a LinkedList of Strings containing the IDs of the nodes from the first node to the target
-     */
-    public LinkedList<String> findNearest(NodeSuper start, String targetType, String condition) {
-        //Initialize class variables
-        this.horizon = new PriorityQueue<AStarNode>(10, new NodeComparator());  //Sorts using NodeComparator
-        this.visitedNodes = new HashMap<>();                                                //Initialized as empty
-        this.horizonNodes = new HashMap<>();                                                //Initialized as empty
-        this.currentNode = new AStarNode(start, start, "NONE", 0);         //Initialized to start node
-        this.targetType = targetType;                           //Initializes the targetType variable
-        this.condition = condition;                             //Initializes the condition variable
-
-        //Initialize and declare local variables
-        LinkedList<String> route = new LinkedList<>();  //Initialized as empty
-        String traceBackNode;                           //Stores the target ID once located
-
-        //Explore the horizon until the a node of the target type is found
-        while(true) {
-            //checkNeighbors adds to the horizon and compares routes, returns false unless the target is found
-            this.visitedNodes.put(this.currentNode.getID(), this.currentNode);
-            traceBackNode = checkNeighbors(false);
-            if(!traceBackNode.equals("false")) {
-                break;
-            }
-            this.currentNode = this.horizon.poll();
-            this.horizonNodes.remove(this.currentNode.getID());
-        }
-
-        //Once the target has been found, retrace steps back to the start node
-        while(true) {
-            route.addFirst(traceBackNode);
-            traceBackNode = this.visitedNodes.get(traceBackNode).getLastID();
-            //Once the start node has been found, break the loop
-            if(traceBackNode == "NONE") {
-                break;
-            }
-        }
-        //return the path (ordered target -> start)
-        return route;
-    }
-
-    /**
-     * function: calcHeuristic()
-     * usage: returns the euclidean distance between two nodes,
-     *        for use in establishing vertex cost
-     * inputs: NodeSuper firstNode (one of the two nodes)
-     *         NodeSuper secondNode (the other node)
-     * returns: int heuristic (the estimated cost to travel from 1 node to the other)
-     */
-    public static int calcHeuristic(NodeSuper firstNode, NodeSuper secondNode) {
-        //calculate euclidean distance between nodes
-        return (int) Math.round(Math.sqrt(Math.pow(firstNode.getXcoord()-secondNode.getXcoord(), 2) + Math.pow(firstNode.getYcoord()-secondNode.getYcoord(), 2)));
     }
 }
---
+++ src/main/java/edu/wpi/MochaManticores/Algorithms/PathPlanning.java
@@ -0,0 +1,31 @@
+package edu.wpi.MochaManticores.Algorithms;
+
+import edu.wpi.MochaManticores.Nodes.NodeSuper;
+
+import java.util.LinkedList;
+
+/**
+ * PathPlanning is an interface containing major navigation functions for AStar, GBF, and Dijkstra
+ * @author aksil
+ */
+public interface PathPlanning {
+
+    /**
+     * method: multiStopRoute()
+     * usage: takes in a linked list of nodes and returns a linked list
+     *        of node IDs representing a path through all input nodes
+     * @param stops LinkedList containing start and target nodes in order
+     * @param condition condition code for the path "publicOnly, handicap, none, publicHandicap"
+     * @return path LinkedList containing all node IDs in the route
+     */
+    LinkedList<String> multiStopRoute(LinkedList<NodeSuper> stops, String condition);
+
+    /**
+     * method: findRoute()
+     * usage: Uses a pathing algorithm to calculate the most efficient route to the target
+     * @param start the node of origin
+     * @param target the node to search for
+     * @return a LinkedList of Strings containing the IDs of the nodes from the first node to the target
+     */
+    LinkedList<String> findRoute(NodeSuper start, NodeSuper target);
+}
