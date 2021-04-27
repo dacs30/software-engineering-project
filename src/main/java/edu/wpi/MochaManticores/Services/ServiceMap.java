b5c4ab202226e27c7d10e5cd9a8ecbf1541512f3
nikopoulospet <peter@nikopoulos.net>
2021-04-27 19:37:28 -0400

    fix for repeated IDs

--- src/main/java/edu/wpi/MochaManticores/Services/ServiceMap.java
+++ src/main/java/edu/wpi/MochaManticores/Services/ServiceMap.java
@@ -6,10 +6,12 @@ import java.util.LinkedList;
 
 public class ServiceMap {
     private final HashMap<ServiceRequestType, HashMap<String, ServiceRequest>> myMap = new HashMap<>();
+    private final HashMap<ServiceRequestType, Integer> maxMap = new HashMap<>();
 
     public ServiceMap() {
         for (ServiceRequestType type : ServiceRequestType.values()) {
             myMap.put(type,new HashMap<String,ServiceRequest>());
+            maxMap.put(type,0);
         }
     }
     /**
@@ -21,6 +23,7 @@ public class ServiceMap {
         //adds a hashmap to key if there is no linked list already there
         myMap.computeIfAbsent(type, k -> new HashMap<String,ServiceRequest>());
         myMap.get(type).put(request.getRequestID(), request);
+        maxMap.put(type,maxMap.get(type)+1);
     }
 
     /**
@@ -61,6 +64,6 @@ public class ServiceMap {
     }
 
     public int getSize(ServiceRequestType type){
-        return myMap.get(type).size();
+        return maxMap.get(type);
     }
 }
