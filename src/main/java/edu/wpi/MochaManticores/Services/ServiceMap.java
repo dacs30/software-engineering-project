826b0dc49359d65cf3b2961357bf8ea5719991b8
kush5683 <32845063+kush5683@users.noreply.github.com>
2021-05-11 23:29:58 -0400

    random request IDS

--- src/main/java/edu/wpi/MochaManticores/Services/ServiceMap.java
+++ src/main/java/edu/wpi/MochaManticores/Services/ServiceMap.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 public class ServiceMap {
     private final HashMap<ServiceRequestType, HashMap<String, ServiceRequest>> myMap = new HashMap<>();
     private final HashMap<ServiceRequestType, Integer> maxMap = new HashMap<>();
-
+    public final LinkedList<Integer> numMap = new LinkedList<>();
     public ServiceMap() {
         for (ServiceRequestType type : ServiceRequestType.values()) {
             myMap.put(type,new HashMap<String,ServiceRequest>());
